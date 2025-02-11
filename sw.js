61654f9f50c1609cbb8753f0d0611a59cb7e5b74
Christina Andersen <64217823+meCodeUp@users.noreply.github.com>
2025-02-11 23:50:57 +0100

    New Web Worker
    Chaching Web Worker
    Add to Index

--- sw.js
+++ sw.js
@@ -1,21 +1,18 @@
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-      caches.open("taskflow-cache").then((cache) => {
-          return cache.addAll([
-              "index.html",
-              "style.css",
-              "script.js",
-              "manifest.json",
-              "icon.png"
-          ]);
-      })
-  );
+// This is the "Offline copy of pages" service worker
+
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-      caches.match(event.request).then((response) => {
-          return response || fetch(event.request);
-      })
-  );
-});
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
